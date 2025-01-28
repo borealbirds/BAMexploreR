eda197d2b569342256bf254e4a7548b9bec01f01
Melina Houle <houle.melina@gmail.com>
2025-01-28 15:48:10 -0500

    Adjust demo url on GD for testing

--- data-raw/vignette-data.R
+++ data-raw/vignette-data.R
@@ -10,14 +10,14 @@ drive_auth()
 ###############################################################
 # version URL data file
 version.url <- tibble(
-  version = c("v4_complete",
-              "v5_complete",
+  version = c("v4_demo",
+              "v5_demo",
               "v4",
               "v5"),
-  url = c("https://drive.google.com/drive/u/0/folders/1aJUZr4fACdD02H8AYejR2XG6zuA6E492",
-          "https://drive.google.com/drive/u/0/folders/1hYkgS0mTIqv7ZWE7t_3TJxsGosQY9eFx",
-          "https://drive.google.com/drive/folders/1bVBEABbrcfgHidmo_PLue916Knjmayre",
-          "https://drive.google.com/drive/folders/1J-V3cdkFYlLolZ53Sy_8tgTDrXLb9xux")
+  url = c("https://drive.google.com/drive/u/0/folders/1bVBEABbrcfgHidmo_PLue916Knjmayre",
+          "https://drive.google.com/drive/u/0/folders/1J-V3cdkFYlLolZ53Sy_8tgTDrXLb9xux",
+          "https://drive.google.com/drive/u/1/folders/1aJUZr4fACdD02H8AYejR2XG6zuA6E492",
+          "https://drive.google.com/drive/u/1/folders/1snHdBwcVyUYCbwYJSCWm5506fiBEYal-")
 )
 
 ###############################################################
