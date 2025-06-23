f2da330217b7da4bc5656e151707954684759ee5
Melina Houle <houle.melina@gmail.com>
2025-06-23 15:52:22 -0400

    Fix test

--- tests/testthat/test-occurenceNM.R
+++ tests/testthat/test-occurenceNM.R
@@ -3,8 +3,9 @@ library(BAMexploreR)
 
 
 test_that("occurrenceNM works with minimal valid raster_list", {
-  r <- terra::rast(matrix(runif(100), 10, 10))
-  result <- occurrenceNM(raster_list = list(r), plot = FALSE)
+  aoi_sf <- vect(system.file("extdata", "vignette_poly_5072.shp", package = "BAMexploreR"))
+  r <- getlayerNM("TEWA", "v5",  crop_ext = aoi_sf, destfile = tempdir(), year = "2020")
+  result <- occurrenceNM(r, plot = FALSE)
   expect_type(result, "list")  # or "double", "S4", depending on return
 })
 
@@ -15,34 +16,32 @@ test_that("occurrenceNM fails with non-raster input", {
 
 
 test_that("occurrenceNM returns expected structure", {
-  r1 <- terra::rast(matrix(runif(100), 10, 10))
-  r2 <- terra::rast(matrix(runif(100), 10, 10))
-  out <- occurrenceNM(raster_list = list(r1, r2), plot = FALSE)
+  aoi_sf <- vect(system.file("extdata", "vignette_poly_5072.shp", package = "BAMexploreR"))
+  r1 <- getlayerNM(c("TEWA", "BBWO"), "v5",  crop_ext = aoi_sf, destfile = tempdir(), year = "2020")
+  out <- occurrenceNM(raster_list = r1, plot = FALSE)
 
-  expect_s3_class(out$population_summary, "data.frame")
-  expect_named(out$population_summary, c("spp", "population_size", "mean_density", "sd_density", "n_cells", "type"))  # adapt to your column names
-  expect_equal(nrow(out$population_summary), 4)
+  expect_s3_class(out$occurrence_summary, "data.frame")
+  expect_named(out$occurrence_summary, c("spp", "type", "area_km2"))  # adapt to your column names
+  expect_equal(nrow(out$occurrence_summary), 4)
+  expect_s4_class(out$occurrence_rasters$TEWA, "SpatRaster")
 })
 
 
 test_that("threshold is within raster value range", {
-  r <- terra::rast(matrix(runif(100, min=0, max=1), 10, 10))
-  out <- occurrenceNM(raster_list = list(r), plot = FALSE)
+  aoi_sf <- vect(system.file("extdata", "vignette_poly_5072.shp", package = "BAMexploreR"))
+  r <- getlayerNM("TEWA", "v5",  crop_ext = aoi_sf, destfile = tempdir(), year = "2020")
+  out <- occurrenceNM(r, plot = FALSE)
 
-  max_val <- max(terra::values(r), na.rm = TRUE)
+  max_val <- max(terra::values(r$TEWA), na.rm = TRUE)
   expect_true(all(out$threshold >= 0 & out$threshold <= max_val))
 })
 
 
-test_that("plot is a ggplot object when plot = TRUE", {
-  r <- terra::rast(matrix(runif(100), 10, 10))
-  p <- occurrenceNM(raster_list = list(r), plot = TRUE)
-  expect_s3_class(p, "ggplot")
-})
-
-
 test_that("different quantile methods yield results", {
-  r <- terra::rast(matrix(runif(100), 10, 10))
-  q1 <- occurrenceNM(raster_list = list(r), quantile = "by_lorenz", plot = FALSE)
-  expect_true(!is.null(q1$threshold))
+  aoi_sf <- vect(system.file("extdata", "vignette_poly_5072.shp", package = "BAMexploreR"))
+  r <- getlayerNM("TEWA", "v5",  crop_ext = aoi_sf, destfile = tempdir(), year = "2020")
+  out <- occurrenceNM(raster_list = r, quantile = "by_lorenz", plot = FALSE)
+  out2 <- occurrenceNM(raster_list = r, quantile = 0.8, plot = FALSE)
+  expect_true(!is.null(out$occurrence_summary))
+  expect_true(!is.null(out2$occurrence_summary))
 })
--- tests/testthat/test-pop_sizeNM.R
+++ tests/testthat/test-pop_sizeNM.R
@@ -1,33 +1,36 @@
 library(testthat)
 library(BAMexploreR)
+library(sf)
+library(terra)
 
-# Check that stackedbarNM handles filtering by species
+# Check that pop_sizeNM handles filtering by species
 test_that("pop_sizeNM handles raster list", {
   rasters <- getlayerNM(c("TEWA", "OVEN"), "v4", destfile=tempdir())
   result <- pop_sizeNM(rasters)
-  expect_s3_class(result, "tbl_df")
-  expect_true(all(c("spp", "population_size", "mean_density", "sd_density", "n_cells") %in% colnames(result)))
+  expect_s3_class(result, "data.frame")
+  expect_true(all(c("species", "total_pop", "mean_density", "sd_density", "n_cells", "group") %in% colnames(result)))
 })
 
 
-# Check that stackedbarNM produces a ggplot object
+# Check that pop_sizeNM correctly sums pixel values
 test_that("pop_sizeNM correctly sums pixel values", {
   m <- matrix(10, 2, 2)
   ras <- list(terra::rast(m))
   names(ras) <- "speciesA"
 
   result <- pop_sizeNM(ras)
-  expect_equal(result$population_size, 4000)  # 10 * 4 pixels
+  expect_equal(result$total_pop, 4000)  # 10 * 4 pixels
   expect_equal(result$mean_density, 10)     # mean of 10s
 })
 
 
-# Test that if groups input is incorrect
+# Test if pop_sizeNM works with list
 test_that("pop_sizeNM works with list of rasters", {
   rasters <- getlayerNM(c("TEWA", "OVEN"), "v4", destfile=tempdir())
   result <- pop_sizeNM(rasters)
   expect_equal(nrow(result), 2)
-  expect_true(all(result$spp %in% c("TEWA", "OVEN")))})
+  expect_true(all(result$spp %in% c("TEWA", "OVEN")))
+})
 
 
 # Test that if groups input is missing
@@ -38,13 +41,60 @@ test_that("pop_sizeNM handles NAs correctly", {
 
   result <- pop_sizeNM(ras)
 
-  expect_equal(result$population_size, sum(c(1, 3, 5))*100)
+  expect_equal(result$total_pop, sum(c(1, 3, 5))*100)
   expect_equal(result$mean_density, mean(c(1, 3, 5)))
 })
 
 
+# Test that it returns an error if input is not a SpatRaster
 test_that("pop_sizeNM throws error on invalid input", {
   expect_error(pop_sizeNM("not a raster"))
 })
 
 
+# Test that Crop work
+test_that("pop_sizeNM throws error on invalid cropping", {
+  rasters <- getlayerNM("TEWA", "v5", destfile=tempdir())
+  result <- pop_sizeNM(rasters, crop_ext=aoi_sf )
+
+  aoi_sf <- vect(system.file("extdata", "vignette_poly_5072.shp", package = "BAMexploreR"))
+  rast_full <- terra::rast(ext = ext(aoi_sf), res = 1080, crs = crs(aoi_sf))
+  values(rast_full) <- 1
+  cropped  <- crop(rast_full, aoi_sf)
+  manual_n <- sum(!is.na(values(cropped)))
+  fn_n       <- result$n_cells[[1]]
+
+  expect_equal(fn_n, manual_n,  tolerance = 0.1, info = glue::glue( "pop_sizeNM reported {fn_n} cells, but manual crop has {manual_n} cells"))
+
+})
+
+# Test that grouping work
+test_that("pop_sizeNM throws error on invalid grouping", {
+  # MN, SK  (approx)
+  mb_poly <- st_as_sf(st_sfc(st_polygon(list(rbind(c(-102, 60),c(-94, 60),c(-94, 49),c(-102, 49),c(-102, 60))))), crs = 4326)
+  sk_poly <- st_as_sf(st_sfc(st_polygon(list(rbind(c(-110, 60),c(-102, 60),c(-102, 49),c(-110, 49),c(-110, 60))))), crs = 4326)
+  poly_5072 <- st_transform(rbind(mb_poly, sk_poly), "EPSG:5072")
+  sv <- vect(poly_5072)
+  sv$id <- c("MB", "SK")
+
+  rasters <- getlayerNM("TEWA", "v4", destfile=tempdir())
+  result <- pop_sizeNM(rasters, crop_ext=sv, group = "id" )
+
+  expect_s3_class(result, "data.frame")
+  expect_equal(nrow(result), 2)
+})
+
+
+# Test if pop_sizeNM works with list
+test_that("pop_sizeNM works with list of rasters while croping and grouping", {
+  mb_poly <- st_as_sf(st_sfc(st_polygon(list(rbind(c(-102, 60),c(-94, 60),c(-94, 49),c(-102, 49),c(-102, 60))))), crs = 4326)
+  sk_poly <- st_as_sf(st_sfc(st_polygon(list(rbind(c(-110, 60),c(-102, 60),c(-102, 49),c(-110, 49),c(-110, 60))))), crs = 4326)
+  poly_5072 <- st_transform(rbind(mb_poly, sk_poly), "EPSG:5072")
+  sv <- vect(poly_5072)
+  sv$id <- c("MB", "SK")
+
+  rasters <- getlayerNM(c("TEWA", "OVEN"), "v4", destfile=tempdir())
+  result <- pop_sizeNM(rasters, crop_ext=sv, group = "id")
+  expect_equal(nrow(result), 4)
+  expect_true(all(result$species %in% c("TEWA", "OVEN")))
+})
