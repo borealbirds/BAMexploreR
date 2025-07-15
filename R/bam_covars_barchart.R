c0467457ab9615a7c707bf69ad5f5b9dd064dd20
ecknight <ellyknight@gmail.com>
2025-07-15 14:17:26 -0600

    variable/covariate --> predictor

--- R/bam_covars_barchart.R
+++ R/bam_covars_barchart.R
@@ -1,7 +1,7 @@
 ##################################################################################
-#' Visualize Covariate Importance as Stacked Bar Charts
+#' Visualize Predictor Importance as Stacked Bar Charts
 #'
-#' Creates stacked bar plots showing the proportion of model covariates importance
+#' Creates stacked bar plots showing the proportion of model predictors importance
 #' in predicting bird abundance.
 #'
 #'
@@ -10,15 +10,15 @@
 #'
 #' @param bcr A \code{character} specifying the model subregions, or Bird Conservation Regions (BCRs) to filter by. The default is \code{"all"}, which includes all BCRs in the dataset.
 #'
-#' @param groups A \code{character} of two grouping variables for summarising covariate importance.
+#' @param groups A \code{character} of two grouping variables for summarising predictor importance.
 #' The first group element is plotted on the x-axis as bins each containing a stacked bar,
 #' while the second group element is shown by fill colours in the stacked bars.
-#' Valid strings are any two of: \code{"spp"} (species), \code{"bcr"} (BCR; model subregion), \code{"var"} (model variable), or \code{"var_class"} (model variable category).
+#' Valid strings are any two of: \code{"spp"} (species), \code{"bcr"} (BCR; model subregion), \code{"predictor"} (model predictor), or \code{"predictor_class"} (model predictor class).
 #' Please see the examples below for a visualization.
 #'
-#' @param version A \code{character}. Defaults to \code{"v5"}. Loads BAM's covariate importance data,
-#' a \code{data.frame} containing covariate importance values, with mean covariate importance as
-#' rows and columns \code{bcr}, \code{species}, \code{var_clas}, \code{n_boot}, \code{mean_rel_inf}, and \code{sd_rel_inf}.
+#' @param version A \code{character}. Defaults to \code{"v5"}. Loads BAM's predictor importance data,
+#' a \code{data.frame} containing predictor importance values, with mean predictor importance as
+#' rows and columns \code{bcr}, \code{species}, \code{predictor_clas}, \code{n_boot}, \code{mean_rel_inf}, and \code{sd_rel_inf}.
 #' \code{"v4"} is also possible but not fully supported for all functions in the first release of this package.
 #'
 #' @param plot A \code{logical} indicating whether to plot the results (\code{TRUE}) or return the processed data (\code{FALSE}).
@@ -26,12 +26,12 @@
 #' @param colours A \code{character} vector of hex codes for the colours to use in the ggplot (optional).
 #' If \code{NULL}, default colours are used.
 #'
-#' @return A stacked bar chart.
+#' @return A stacked bar chart with the first group element plotted on the x-axis as bins each containing a stacked bar, and the second group element is shown by fill colours in the stacked bars.  If plot = FALSE the processed data is returned as a data.frame.
 #'
-#' @details Stacked bars can be grouped by species, variable class, or
-#' Bird Conservation Region (BCR). For example, grouping by species and variable class creates
+#' @details Stacked bars can be grouped by species, predictor class, or
+#' Bird Conservation Region (BCR). For example, grouping by species and predictor class creates
 #' a plot where a stacked bar is created for each species, and each bar is split into the proportion
-#' of covariate importance that each of nine variable classes contributed, pooled across the specified BCRs.
+#' of predictor importance that each of nine predictor classes contributed, pooled across the specified BCRs.
 #'
 #'
 #' @importFrom rlang syms
@@ -42,38 +42,38 @@
 #'
 #' @examples
 
-#' # Compare covariate importance (binned by variable class) for all species in all BCRs
-#' bam_covars_barchart(species = "all", bcr = "all",  groups = c("spp", "var_class"))
+#' # Compare predictor importance (binned by predictor class) for all species in all BCRs
+#' bam_covars_barchart(species = "all", bcr = "all",  groups = c("spp", "predictor_class"))
 #'
-#' # Compare covariate importance (binned by variable class) in the Prairies (BCRs 11, 6-1, 6-0)
+#' # Compare predictor importance (binned by predictor class) in the Prairies (BCRs 11, 6-1, 6-0)
 #' # to the Pacific Coast across (BCR 5) all species
 #' prairies_to_coast <- c("can11", "can60", "can61", "can5")
-#' bam_covars_barchart(species = "all", bcr = prairies_to_coast, groups=c("bcr", "var_class"))
+#' bam_covars_barchart(species = "all", bcr = prairies_to_coast, groups=c("bcr", "predictor_class"))
 #'
-#' # Compare covariate importance (binned by variable class) for four
+#' # Compare predictor importance (binned by predictor class) for four
 #' # warbler species in BCR14
 #' warblers <- c("CAWA", "BAWW", "BTNW", "BLBW")
-#' bam_covars_barchart(species = warblers, bcr = "can14", groups = c("spp", "var_class"))
+#' bam_covars_barchart(species = warblers, bcr = "can14", groups = c("spp", "predictor_class"))
 #'
-#' # Compare covariate importance for a single warbler species
-#' # relative to the total influence that covariate had across all warblers.
-#' bam_covars_barchart(species = warblers, bcr = "can14", groups = c("var", "spp"))
+#' # Compare predictor importance for a single warbler species
+#' # relative to the total influence that predictor had across all warblers.
+#' bam_covars_barchart(species = warblers, bcr = "can14", groups = c("predictor", "spp"))
 
 
-bam_covars_barchart <- function(species = "all", bcr = "all",  groups = c("spp", "var_class"), version ="v5", plot = TRUE, colours = NULL){
+bam_covars_barchart <- function(species = "all", bcr = "all",  groups = c("spp", "predictor_class"), version ="v5", plot = TRUE, colours = NULL){
 
   if (!version %in% c("v4", "v5")) {
     stop("Invalid version argument. Must be either 'v4' or 'v5'.")
   }
 
-  # load bam_covariate_importance_v* from data folder
+  # load bam_predictor_importance_v* from data folder
   #load(system.file("R/sysdata.rda", package = "BAMexploreR"))
   if (version == "v5") {
-    #data("bam_covariate_importance_v5", package = "BAMexploreR")
-    data <- bam_covariate_importance_v5
+    #data("bam_predictor_importance_v5", package = "BAMexploreR")
+    data <- bam_predictor_importance_v5
   } else {
-    #data("bam_covariate_importance_v4", package = "BAMexploreR")
-    data <- bam_covariate_importance_v4
+    #data("bam_predictor_importance_v4", package = "BAMexploreR")
+    data <- bam_predictor_importance_v4
   }
 
   # check if user specified species are in `data`
@@ -114,7 +114,7 @@ bam_covars_barchart <- function(species = "all", bcr = "all",  groups = c("spp",
   group_syms <- rlang::syms(groups)
 
 
-  # sum covariate importance across for every permutation of group1 and group2
+  # sum predictor importance across for every permutation of group1 and group2
   rel_inf_sum <-
     data |>
     drop_na() |>
@@ -122,13 +122,13 @@ bam_covars_barchart <- function(species = "all", bcr = "all",  groups = c("spp",
     summarise(sum_influence = sum(mean_rel_inf), .groups="keep")
 
 
-  # sum of covariate importance for each of group1 (all group2 sums are amalgamated into group1 bins)
+  # sum of predictor importance for each of group1 (all group2 sums are amalgamated into group1 bins)
   group1_sum <-
     rel_inf_sum |>
     group_by(!!group_syms[[1]])  |>
     summarise(sum_group1 = sum(sum_influence), .groups="keep")
 
-  # get the %contribution of group2 covariates to overall covariate importance for a given group1
+  # get the %contribution of group2 predictors to overall predictor importance for a given group1
   proportion_inf <-
     rel_inf_sum |>
     left_join(x = _, group1_sum, by=groups[1]) |>
--- R/bam_covars_importance.R
+++ R/bam_covars_importance.R
@@ -1,18 +1,18 @@
 ##################################################################################
-#' Plot Side-by-Side Variation in Covariate Importance by Variable Class
+#' Plot Side-by-Side Variation in Covariate Importance by Predictor Class
 #'
-#' @description Plot mean relative covariate importance (with bootstrap variation)
-#' by variable class, with options to group by BCR (Bird Conservation Region) or species.
+#' @description Plot mean relative predictor importance (with bootstrap variation)
+#' by predictor class, with options to group by BCR (Bird Conservation Region) or species.
 #'
 #' @param species A \code{character} specifying the species to filter by. The default is \code{"all"}, which includes all species in the dataset.
 #'
 #' @param bcr A \code{character} specifying the Bird Conservation Regions (BCRs) to filter by. The default is \code{"all"}, which includes all BCRs in the dataset.
 #'
-#' @param group A \code{character} specifying the grouping variable for summarizing covariate importance.
+#' @param group A \code{character} specifying the grouping variable for summarizing predictor importance.
 #' Valid strings are \code{"spp"} (species), or \code{"bcr"} (BCR).
 #'
-#' @param version A \code{character}. Defaults to \code{"v5"}. Loads BAM's covariate importance data,
-#' a \code{data.frame} containing covariate importance values, with mean covariate importance as
+#' @param version A \code{character}. Defaults to \code{"v5"}. Loads BAM's predictor importance data,
+#' a \code{data.frame} containing predictor importance values, with mean predictor importance as
 #' rows and columns \code{bcr}, \code{species}, \code{var_class}, \code{n_boot}, \code{mean_rel_inf}, and \code{sd_rel_inf}.
 #' \code{"v4"} is also possible but not fully supported for all functions in the first release of this package.
 #'
@@ -21,9 +21,9 @@
 #' @param colours A \code{character} vector of hex codes for the colours to use in the ggplot (optional).
 #' If \code{NULL}, default colours are used.
 #'
-#' @return A ggplot displaying percent covariate importance by variable class, grouped by the \code{group} argument.
+#' @return A ggplot displaying percent predictor importance by predictor class, grouped by the \code{group} argument.
 #' Percent importance is used to allow comparisions across groups that have
-#' differing total covariate importance.
+#' differing total predictor importance.
 #' If \code{plot = FALSE} the processed data is returned as a \code{data.frame}.
 #'
 #' @details Bootstrap variation (per species x BCR) is propagated by
@@ -38,11 +38,11 @@
 #' @examples
 #'
 #'
-#' # Example of plotting covariate importance for Townsend's Solitaire across all BCRs.
+#' # Example of plotting predictor importance for Townsend's Solitaire across all BCRs.
 #' # This is a species with relatively high bootstrap variance.
 #' bam_covars_importance(species = "TOSO")
 #'
-#' # Example of plotting covariate importance for two warbler species from three BCRs,
+#' # Example of plotting predictor importance for two warbler species from three BCRs,
 #' # using custom colours:
 #' bam_covars_importance(species = c("BAWW", "CAWA"), group = "spp",
 #' bcr = c("can12", "can13", "can14"),  colours = c("#1f78b4", "#33a02c"))
@@ -58,14 +58,14 @@ bam_covars_importance <- function(species = "all", bcr = "all", group = "spp", v
     stop("Invalid `version`. Use 'v4' or 'v5'")
   }
 
-  # load bam_covariate_importance_v* from data folder
+  # load bam_predictor_importance_v* from data folder
   #load(system.file("R/sysdata.rda", package = "BAMexploreR"))
   if (version == "v5") {
-    #data("bam_covariate_importance_v5", package = "BAMexploreR")
-    data <- bam_covariate_importance_v5
+    #data("bam_predictor_importance_v5", package = "BAMexploreR")
+    data <- bam_predictor_importance_v5
   } else {
-    #data("bam_covariate_importance_v4", package = "BAMexploreR")
-    data <- bam_covariate_importance_v4
+    #data("bam_predictor_importance_v4", package = "BAMexploreR")
+    data <- bam_predictor_importance_v4
   }
 
   # check if user specified species are in `data`
@@ -110,7 +110,7 @@ bam_covars_importance <- function(species = "all", bcr = "all", group = "spp", v
 
 
   # group by user-specified group
-  # then, sum rel.inf by the grouped variable per variable class
+  # then, sum rel.inf by the grouped predictor per predictor class
   # convert std. dev. back to variance, sum, and take sqrt()
   cov_importance_grouped <-
     data |>
@@ -127,7 +127,7 @@ bam_covars_importance <- function(species = "all", bcr = "all", group = "spp", v
     group_by(!!group_sym[[1]]) |>
     summarise(sum_all_groups = sum(sum_inf), .groups = "keep")
 
-  # calculate the percent of covariate importance
+  # calculate the percent of predictor importance
   # sd_percent_inf is the uncertainty of the percent influence of a given var_class
   percent_importance <-
     cov_importance_grouped |>
@@ -142,7 +142,7 @@ bam_covars_importance <- function(species = "all", bcr = "all", group = "spp", v
       geom_point(position = position_dodge(width = 0.75), alpha = 0.7, size = 2.5) +
       geom_errorbar(aes(ymax = percent_inf + sd_percent_inf, ymin = percent_inf - sd_percent_inf),
                     position = position_dodge(width = 0.75), width = 0, linewidth = 0.75) +
-      labs(x = "Variable Class", y = "Relative Importance (%)",
+      labs(x = "Predictor Class", y = "Relative Importance (%)",
            title = paste("Covariate importance by", group)) +
       theme_classic() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
