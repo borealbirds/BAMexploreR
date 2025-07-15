1647b416c453dcea6ac66dd78ed922a917550e86
ecknight <ellyknight@gmail.com>
2025-07-15 15:57:15 -0600

    updating predictor terminology

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c(".", ".env","var_class", "mean_rel_inf", "sd_rel_inf", "species", "spp", "sum_inf", "sum_all_groups",
+  utils::globalVariables(c(".", ".env","predictor_class", "mean_rel_inf", "sd_rel_inf", "species", "spp", "sum_inf", "sum_all_groups",
                            "pooled_sd", "percent_inf", "sym", "sd_percent_inf", "guild_opt", "speciesCode", "commonName",
                            "scientificName", "sum_influence", "sum_group1", "prop", "density", "spp_tbl"))
 }
